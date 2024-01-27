bc95bd1ea12679414a91854461aa17e3a5895338
Manoel Prado <manoel.prado.neto@accenture.com>
2024-01-27 18:27:57 -0300

    create Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import { HeaderContainer, HeaderContent, NewTransactionBtn } from "./styles";
+
+import logoImg from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} />
+
+        <NewTransactionBtn>Nova transação</NewTransactionBtn>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
