1d9b7476fdc2281a45e1aa222d59884ef447267d
Manoel Prado <manoel.prado.neto@accenture.com>
2024-01-27 20:29:02 -0300

    creating accessible modal with radix-ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionBtn } from "./styles";
 
 import logoImg from '../../assets/logo.svg'
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
@@ -8,7 +9,20 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} />
 
-        <NewTransactionBtn>Nova transação</NewTransactionBtn>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionBtn>Nova transação</NewTransactionBtn>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay/>
+
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <Dialog.DialogClose />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
