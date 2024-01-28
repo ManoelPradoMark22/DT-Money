444359a6293fd6b08c22c73a905cf66d40360be0
Manoel Prado <manoel.prado.neto@accenture.com>
2024-01-28 17:23:04 -0300

    calculate the transactions summary using .reduce()

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,8 +7,22 @@ import { TransactionsContext } from "../../contexts/TransactionsContext";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
-  console.log('Summary')
-  console.log(transactions)
+  const summary = transactions.reduce((acc, transaction) => {
+    if(transaction.type === 'income') {
+      acc.income += transaction.price
+      acc.total += transaction.price
+    } else {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+    }
+    
+    return acc
+  },
+  {
+    income: 0,
+    outcome: 0,
+    total: 0
+  })
 
   return (
     <SummaryContainer>
@@ -18,7 +32,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color={defaultTheme['green-300']}/>
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
@@ -26,7 +40,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color={defaultTheme['red-300']}/>
         </header>
 
-        <strong>R$ 1.259,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
       <SummaryCard variant='green'>
         <header>
@@ -34,7 +48,7 @@ export function Summary() {
           <CurrencyDollar size={32} color={defaultTheme['white']}/>
         </header>
 
-        <strong>R$ 16.141,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
